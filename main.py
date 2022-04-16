ab4a3c7961239cffba2db8420866d45a4ee22f24
Jibran Absarulislam <jga26@njit.edu>
2022-04-15 23:40:33 -0400

    refactor by putting in functions and standards

--- main.py
+++ main.py
@@ -1,55 +1,87 @@
 from subprocess import call
 import os
+import sys
 
-# main.py is intended to be run from the terminal
-hashes = input("Word list file: ")                                                  # i.e.: rockyou.txt
-
-while True:
-    user_pass_file_dir = input("What is the name of the file you want to crack? ")  # i.e.: originalfile.txt
-    # note: originalfile.txt is intended to contain lines with format username:password_hash
-    content = []                                                                    # initialize empty array
-    try:
-        with open(user_pass_file_dir) as f:                                         # open originalfile.txt
-            content = f.readlines()                                                 # populate content arr with a line per index
-        content = [x.strip() for x in content]
-    except:
-        print("Please enter a valid file directory")
-    #file.close()                                                                   # close originalfile.txt (source file)
-
-    output_file_name = "parsed_hashes.txt"                                          # indiciate filename of just hashes to look at
-    new_file = open(output_file_name, "w")                                          # designate new_file as a to-write file with name parsed_hashes
-    for line in content:                                                            # for each line stored in the indixes of content
-        line_arr = line.split(":")                                                  # local variable line_arr set to [username, password_hash]
-        new_file.write(line_arr[1]+"\n")                                            # write just the password_hash to parshed_hashes
-    new_file.close()                                                                # close new_file.txt
-
-    os.chdir("hashcat-6.2.2")                                                       # change file access to the hashcat directory
-    print("\nHere's what was found...\n")
-    pot_file = "outfile.txt"                                                        # hash : pass
-    with open(pot_file, "w") as pot:                                                # open
-        pot.truncate(0)                                                             # clear so not to overwrite
-    call(["./hashcat.exe", "-m", "0", "../" + output_file_name, "../" + hashes, "-o", pot_file, "--potfile-disable"]) # call(...) is as if you're typing the arguments in the cmd line
-    break
-
-print("\nUsername%-*sPassword%s" % (20,"",""))                                      # "table" formatting
-print("========================================")
-out_content = []                                                                    # store outfile contents in out_content
-with open("outfile.txt") as new_f:
-    out_content = new_f.readlines()
-out_content = [x.strip() for x in out_content]                                      # remove whitespace
-
-hash = []
-for line in out_content:
-    hash += line.split(":")
-
-for line_num in range(len(content)):
-    lst = content[line_num].split(":")
-                                                                                    # print(lst) #lst[1] stores the hash and hash[1]
-    i = 0
-    if lst[1] in hash:
-        print("%-*s %s" % (20, lst[0], hash[hash.index(lst[1])+1]))
+def get_file_to_crack():
+    hashes = os.path.abspath('rockyou.txt')
+    if not os.path.isfile(hashes):
+        hashes = input("Word list file: ")
     else:
-        print("%-*s %s" % (20, lst[0], "Not found in table"))
-    i += 1
-f.close()
-new_f.close()
+        print(f'rockyou.txt found at {hashes}\n')
+    
+    return hashes
+
+def print_ls():
+    curr_dir = os.listdir()
+    for f in curr_dir:
+        print(f)
+
+def read_db_hashes(filename):
+    content = []
+    while True:
+        try:
+            with open(filename) as f:
+                content = f.readlines()
+            content = [x.strip() for x in content]
+            break
+        except:
+            print("Please enter a valid file directory")
+
+def send_to_hashcat(output_file_name, hashes):
+    os.chdir("hashcat-6.2.2")
+    print("\nHere's what was found...\n")
+    pot_file = "outfile.txt"
+    with open(pot_file, "w") as pot:
+        pot.truncate(0)
+    call(["./hashcat.exe", "-m", "0", "../" + output_file_name,
+            "../" + hashes, "-o", pot_file, "--potfile-disable"])
+
+def parse_cracked():
+    out_content = []
+    with open("outfile.txt") as new_f:
+        out_content = new_f.readlines()
+    out_content = [x.strip() for x in out_content]
+
+    hash = []
+    for line in out_content:
+        hash.append(line.split(":"))
+    
+    return hash
+
+def print_formatted(content):
+    print("%-*s %s" % (20, lst[0], content))
+def main():
+    hashes = get_file_to_crack()
+    while True:
+        print_ls()
+
+        user_pass_file_dir = input(
+            "\nWhat is the name of the file you want to crack? ")
+
+        content = read_db_hashes(user_pass_file_dir)
+
+        output_file_name = "parsed_hashes.txt"
+        with open(output_file_name, 'w') as new_file:
+            for line in content:
+                line_arr = line.split(":")
+                new_file.write(line_arr[1]+"\n")
+
+        send_to_hashcat(output_file_name, hashes)
+        break
+
+    print("\nUsername%-*sPassword%s" % (20, "", ""))
+    print("========================================")
+    
+    hash = parse_cracked()
+
+    for value in content:
+        lst = value.split(":")
+        if lst[1] in hash:
+            print_formatted(hash[hash.index(lst[1])+1])
+        else:
+            print_formatted("Not found in table")
+
+
+
+if __name__ == '__main__':
+    main()
